c88b569c0dc225846b956aa0fe7555d9b9c9c725
MariamHasanat <201011@ppu.edu.ps>
2024-05-04 10:44:51 +0300

    listen changes on port 3001

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const mongoose = require('mongoose')
 
 const app = express();
 
+app.use(express.json());
+
+app.listen(3001, ()=>{
+    console.log("Hello Mariam , server running successfully ");
+})
