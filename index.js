00e84d3d83f90caf223e46833747e50e0d091779
MariamHasanat <201011@ppu.edu.ps>
2024-05-04 11:19:31 +0300

    databases running successfully

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose')
+require('dotenv').config();
 
+const dbConnect = () => {
+    mongoose.connect("mongodb://127.0.0.1:27017/My_Note_Keeper")
+        .then(
+            () => {
+                console.log(`⚡️ [server] : connected to mongodb`);
+            }
+        )
+        .catch((error) => {
+            console.log(`⚡️ [server] : failed to connect to mongodb ${error}`);
+        })
+};
 const app = express();
 
 app.use(express.json());
 
-app.listen(3001, ()=>{
+app.listen(3001, () => {
     console.log("Hello Mariam , server running successfully ");
+    dbConnect();
 })
